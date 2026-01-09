6b15352a351378ad31b5399e2641ff4ce3b88a1e
gani-gani <ganeshvg45@gmail.com>
2026-01-09 07:54:46 +0530

    backend fix

--- new-mart-backend/server.js
+++ new-mart-backend/server.js
@@ -26,4 +26,11 @@ app.use("/api/products", productRoutes);
 app.use("/api/testimonials", testimonialRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on ${PORT}`));
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () =>
+    console.log(`🚀 Server running on http://localhost:${PORT}`)
+  );
+}
+
+export default app;
